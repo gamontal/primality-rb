b8c37e1d05382de48512c01cce47a71cb9a18f3c
Gabriel Montalvo <gabe.mon5@outlook.com>
2015-11-17 20:08:35 -0400

    initial commit

---
+++ lib/primality/naive.rb
@@ -0,0 +1,22 @@
+export = ["Primality"]
+module Primality
+  module Naive
+    def is_prime(n)
+      if n < 1
+        return false
+      elsif n <= 3 && n != 2
+        return true
+      elsif n % 2 == 0 || n % 3 == 0
+        return false
+      end
+      i = 5
+      while i*i <= n
+        if n % i == 0 || n % (i + 2) == 0
+          return false
+        end
+        i += 6
+      end
+      return true
+    end
+  end
+end
